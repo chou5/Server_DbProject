4c6f0901353c3b3ffaa113f4e0fcdeab01fe6057
Chiahsuanchou <chou5@iu.umail.edu>
2016-12-01 02:37:30 +0800

    successfully insert employee data in Employee Table

--- flysheetDb_data.py
+++ flysheetDb_data.py
@@ -4,18 +4,15 @@ import openpyxl
 db = sqlite3.connect('flysheetDb.db')
 wb = openpyxl.load_workbook('data.xlsx')
 employee = wb['EMPLOYEE']
-#employee_list = []
 for idx, row in enumerate(employee.rows):
-	per_row = []
 	if idx != 0:
+		per_row = []
 		for i, cell in enumerate(row):
-			if i != 0:
-				per_row.append(cell.value)
-			#print per_row
-			db.execute('INSERT INTO Employee(fname,lname,title,department,base_region,gender,dob,email,phone,start_date) VALUES (?,?,?,?,?,?,?,?,?,?)', tuple(per_row))   
-		#employee_list.append(per_row)
+			per_row.append(cell.value)
+		del per_row[0]
+		db.execute('INSERT INTO Employee(fname,lname,title,department,base_region,gender,dob,email,phone,start_date) VALUES (?,?,?,?,?,?,?,?,?,?)', tuple(per_row))   
 
-#print employee_list
+
 
 
 db.commit()
